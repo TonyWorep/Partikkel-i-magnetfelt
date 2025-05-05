ed4417d6797f674e9071e08edcee2825e2c34a84
Ask <legoask@gmail.com>
2025-05-05 14:49:27 +0200

    feat: vektorprodukt

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 def main():
-    return True
+    B = [0, 0, 1]
+    p = [0, 0, 0]
+    v = [1, 0, 0]
+
+    def vektorprodukt(v1, v2):
+        x = v1[1] * v2[2] - v1[2] * v2[1]
+        y = -(v1[0] * v2[2] - v1[2] * v2[0])
+        z = v1[0] * v2[1] - v1[1] * v2[0]
 
 if __name__ == "__main__":
     main()
