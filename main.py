525fbbc6a19a05c82e296803c59c50cc89a2b775
TonyWorep <tonyworep@gmail.com>
2025-05-07 10:21:40 +0200

    feat: grafisk framstilling i 3d

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from matplotlib import pyplot as plt
+
+
 def main():
-    B = [0, 0, 1e-3]
+    B = [1e-3, 1e-3, 1e-3]
     p = [0, 0, 0]
     v = [0, 3.58e4, 0]
     q = 1.67e-19
     m = 9.11e-31
 
-    N = int(1000)
+    N = 1000
     T = 0.0000001
     dt = T/N
 
@@ -38,21 +40,18 @@ def main():
         v = fart(a, v, dt)
         p = pos(v, p, dt)
 
-
-        
-
         x.append(p[0])
         y.append(p[1])
         z.append(p[2])
-    
-    #print(x)
-    #print()
-    #print(y)
-    #print()
-    #print(z)
-    plt.plot(x, y)
-    plt.show()
 
+    fig = plt.figure()
+    ax = plt.axes(projection="3d")
+    ax.plot3D(x, y, z)
+    ax.set_xlabel("X/m")
+    ax.set_ylabel("Y/m")
+    ax.set_zlabel("Z/m")
+    ax.set_title("Partikkel i magnetfelt")
+    plt.show()
 
 
 if __name__ == "__main__":
