24634e9787aa2e6cf529d38d498eb1f1d1ef39d8
TonyWorep <tonyworep@gmail.com>
2025-05-07 10:27:33 +0200

    feat: refactor code

---
+++ calculations.py
@@ -0,0 +1,23 @@
+def kraft(v1, v2, q, m):
+    x = v1[1] * v2[2] - v1[2] * v2[1]
+    y = -(v1[0] * v2[2] - v1[2] * v2[0])
+    z = v1[0] * v2[1] - v1[1] * v2[0]
+    return [(q*x)/m, (q*y)/m, (q*z)/m]
+
+def fart(a, v, dt):
+    vx = v[0] + a[0]*dt
+    vy = v[1] + a[1]*dt
+    vz = v[2] + a[2]*dt
+    return [vx, vy, vz]
+
+def pos(v, p, dt):
+    px = p[0] + v[0]*dt
+    py = p[1] + v[1]*dt
+    pz = p[2] + v[2]*dt
+    return [px, py, pz]
+
+def main():
+    print("Wrong here bud.")
+
+if __name__ == "__main__":
+    main()
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from matplotlib import pyplot as plt
 
+from calculations import fart, kraft, pos
+
 
 def main():
     B = [1e-3, 1e-3, 1e-3]
@@ -15,25 +17,6 @@ def main():
     x = []
     y = []
     z = []
-
-    def kraft(v1, v2, q, m):
-        x = v1[1] * v2[2] - v1[2] * v2[1]
-        y = -(v1[0] * v2[2] - v1[2] * v2[0])
-        z = v1[0] * v2[1] - v1[1] * v2[0]
-        return [(q*x)/m, (q*y)/m, (q*z)/m]
-    
-    def fart(a, v, dt):
-        vx = v[0] + a[0]*dt
-        vy = v[1] + a[1]*dt
-        vz = v[2] + a[2]*dt
-        return [vx, vy, vz]
-    
-    def pos(v, p, dt):
-        px = p[0] + v[0]*dt
-        py = p[1] + v[1]*dt
-        pz = p[2] + v[2]*dt
-        return [px, py, pz]
-
     
     for i in range (N):
         a = kraft(v, B, q, m)
